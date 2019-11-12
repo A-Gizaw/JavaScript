690d95a12882143eea651795dc8d08c6fd337cd5
Adam Gizaw <adam.gizaw@ynap.com>
2019-11-12 14:27:21 +0000

    added anonymous functions

--- functions.js
+++ functions.js
@@ -34,3 +34,40 @@ function isCaptainOfStrawHats(name){
 
 console.log(isCaptainOfStrawHats('Luffy'));//prints 'Luffy SENCHO !!!!''
 console.log(isCaptainOfStrawHats('Usopp'));//prints 'Anata wa sencho janai...kuso yaro'
+
+/********    ANONYMOUS FUNCTIONS    ********/
+
+/*
+If a function is not going to be used many times then an ANONYMOUS function would be prefreble.
+  This takes up less memory as it may not be excecuted / assigned a location in memory
+
+It's common practice is to assign it to a CONST variable , however you can use any.
+*/
+
+const shout =  function(){
+  return 'I\'M GOING TO BE THE KING OF THE PIRATES'
+}
+
+//Do not forget paramenter when callin the anonymous function
+console.log(shout); //prints out the function refrence
+console.log(shout()); //Runs the actual function
+
+var dreams = function(name){
+  let dream
+  switch(name){
+    case 'luffy': dream = 'I\'M GOING TO BE THE KING OF THE PIRATES!!!'
+                break;
+    case 'zoro': dream = 'I\'m going to be the worlds greates swordsman'
+                break;
+    case 'sanji': dream ='I\'m going to find the all blue'
+                break;
+    default:dream = 'Find the one piece'
+            break;
+  }
+  return dream
+}
+
+console.log(dreams());
+console.log(dreams('luffy'));
+console.log(dreams('zoro'));
+console.log(dreams('sanji'));
